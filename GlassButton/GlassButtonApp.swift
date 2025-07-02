0ebfd5f412a47835f6891157fc07aa9512acfcba
Vikas Raj Yadav <vikasyadav.vision111@gmail.com>
2025-07-02 19:09:43 +0530

    Initial Commit

---
+++ GlassButton/GlassButtonApp.swift
@@ -0,0 +1,17 @@
+//
+//  GlassButtonApp.swift
+//  GlassButton
+//
+//  Created by Vikas Raj Yadav on 02/07/25.
+//
+
+import SwiftUI
+
+@main
+struct GlassButtonApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
