69862c1d480ea6272c1dee720d6b3f096d1fab94
Vikas Raj Yadav <vikasyadav.vision111@gmail.com>
2025-07-15 18:13:02 +0530

    More shadow and glass.

---
+++ GlassButton/GlassText.swift
@@ -0,0 +1,18 @@
+//
+//  GlassText.swift
+//  GlassButton
+//
+//  Created by Vikas Raj Yadav on 12/07/25.
+//
+
+import SwiftUI
+
+struct GlassText: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    GlassText()
+}
